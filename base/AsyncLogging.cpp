2bb06d21af66f9d60b9fe1e97254547e3ca5a0f5
zk_glb <1552535580@qq.com>
2024-03-19 23:59:10 +0800

    3.19.1

--- base/AsyncLogging.cpp
+++ base/AsyncLogging.cpp
@@ -49,7 +49,7 @@ void AsyncLogging::threadFunc() {
     BufferPtr newBuffer2(new Buffer);
     newBuffer1->bzero();
     newBuffer2->bzero();
-    BufferVector buffersToWrite;
+    BufferVector buffersToWrite;//该vector属于后端线程，用于和前端的buffer进行交换
     buffersToWrite.reserve(16);
     while (running_) {
         assert(newBuffer1 && newBuffer1->length() == 0);
@@ -73,7 +73,8 @@ void AsyncLogging::threadFunc() {
         }
 
         assert(!buffersToWrite.empty());
-
+//如果队列中buffer数⽬⼤于25，就删除多余数据
+        //避免日志堆积：前端日志记录过快，后端来不及写入文件
         if (buffersToWrite.size() > 25) {
             // char buf[256];
             // snprintf(buf, sizeof buf, "Dropped log messages at %s, %zd larger
@@ -82,6 +83,8 @@ void AsyncLogging::threadFunc() {
             //          buffersToWrite.size()-2);
             // fputs(buf, stderr);
             // output.append(buf, static_cast<int>(strlen(buf)));
+            //TODO:删除数据是加错误提示
+            //只留原始的两个buffer，其余的删除
             buffersToWrite.erase(buffersToWrite.begin() + 2, buffersToWrite.end());
         }
 
@@ -90,6 +93,7 @@ void AsyncLogging::threadFunc() {
             output.append(buffersToWrite[i]->data(), buffersToWrite[i]->length());
         }
 
+        //重新调整buffertowhrite的大小，仅保留两个原始buffer
         if (buffersToWrite.size() > 2) {
             // drop non-bzero-ed buffers, avoid trashing
             buffersToWrite.resize(2);
@@ -101,6 +105,11 @@ void AsyncLogging::threadFunc() {
             buffersToWrite.pop_back();
             newBuffer1->reset();
         }
+        //因为非空指针的话是true，然后！new得到的是true，说明原来的那个是空的
+        //首先，代码使用 if 语句检查 newBuffer1 是否为空指针，即是否指向 nullptr。当 newBuffer1 为空时，执行 if 语句块中的代码。
+        //在 if 语句块中，第一行使用了 assert 断言来验证一个条件，即 buffersToWrite 是否为空。assert 断言用于在代码中插入调试断点，用于确保某个条件为真，如果条件为假，则触发断言失败，输出错误信息并终止程序。
+        //如果 buffersToWrite 不为空，则继续执行下面的代码。第三行将 newBuffer1 的指针指向 buffersToWrite 中的最后一个元素，即 buffersToWrite.back()。然后，通过 pop_back() 函数将 buffersToWrite 中的最后一个元素移除。
+        //最后一行调用 newBuffer1 的 reset() 函数，对其进行重置操作。reset() 函数的具体含义可能根据代码上下文而有所不同，但通常用于清空或重置对象的状态或成员变量。
 
         if (!newBuffer2) {
             assert(!buffersToWrite.empty());
